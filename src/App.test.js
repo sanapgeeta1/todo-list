ac2837b7d28d117c09d70ee20c103320ea05e50d
sanapgeeta1 <42575046+sanapgeeta1@users.noreply.github.com>
2024-01-23 02:31:34 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
+import React from "react";
+import App from './App';
+import { render, screen } from '@testing-library/react';
+
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/To Do/i);
   expect(linkElement).toBeInTheDocument();
 });
