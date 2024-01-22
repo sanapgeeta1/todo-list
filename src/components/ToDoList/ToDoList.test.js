f42e4c84d62772a27df0665c3d4a122577ec2f91
sanapgeeta1 <42575046+sanapgeeta1@users.noreply.github.com>
2024-01-23 02:31:00 +0530

    Update ToDoList.test.js

--- src/components/ToDoList/ToDoList.test.js
+++ src/components/ToDoList/ToDoList.test.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from '@testing-library/react';
+
 import ToDoList from "./ToDoList";
 
-describe("ToDoList", () => {
-  test("matches snapshot", () => {
-    const wrapper = shallow(<ToDoList />);
-    expect(wrapper).toMatchSnapshot();
-  });
-});
+
+
+test("ToDoList Componant renders successfully", () => {
+  render(<ToDoList />);
+  const element = screen.getByText(/To Do List App/i);
+  expect(element).toBeInTheDocument();
+})
+
+
+//data render
+test("List completed successfully",  async () => {
+  render(<ToDoList />);
+  const ele = screen.getByText(/Completed/i);
+  expect(ele).toBeInTheDocument();
+})
+//data render
+test("Add To Do List",  async () => {
+  render(<ToDoList />);
+  const ele = screen.getByText(/Add/i);
+  expect(ele).toBeInTheDocument();
+})
+
+test("Button disabled on Add click", async () => {
+  render(<ToDoList />);
+  const ele = screen.getByText(/Add/i);
+  expect(screen.getByText(/Add/i)).toBeDisabled();
+})
+
+
